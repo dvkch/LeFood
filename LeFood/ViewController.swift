22b1806c22bf9092486fc062d17009fa8e4884b9
Stanislas Chevallier <contact@stanislaschevallier.fr>
2024-05-13 20:40:51 +0200

    Initial Commit

---
+++ LeFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeFood
+//
+//  Created by syan on 13/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
